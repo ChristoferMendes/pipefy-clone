2869db8667a365ad1a2a3f8e0f79af1978c3bd97
ChristoferMendes <christoferluizdsm@gmail.com>
2023-06-02 15:31:20 -0300

    add buses so the app doesn't do unnecessary calculations

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,16 +1,59 @@
+import { useRef } from "react";
 import { Container, Label } from "./styles.js";
-import { useDrag } from "react-dnd";
+import { useDrag, useDrop } from "react-dnd";
 
-export function Card({ data }) {
+export function Card({ data, targetIndex }) {
+  const ref = useRef();
   const [{ isDraggin }, dragRef] = useDrag({
     type: 'CARD',
+    item: {
+      // id: data.id,
+      targetIndex,
+      // content: data.content
+    },
     collect: (monitor) => ({
       isDraggin: monitor.isDragging(),
     }),
   });
 
+  const [, dropRef] = useDrop({
+    accept: 'CARD',
+    hover(item, monitor) { 
+      const draggedIndex = item.targetIndex;
+
+      const isUserDraginTheCardInsideTheSame = draggedIndex === targetIndex;
+
+      if (isUserDraginTheCardInsideTheSame) return;
+
+      const targetSize = ref.current.getBoundingClientRect();
+      const targetCenter = (targetSize.bottom - targetSize.top) / 2;
+
+      const draggedOffset = monitor.getClientOffset();
+
+      const draggedTop = draggedOffset.y - targetSize.top;
+      
+      const draggedItemComesBefore = draggedIndex < targetIndex
+      const draggedItemIsAboveTarget = draggedTop < targetCenter
+
+      if (draggedItemComesBefore && draggedItemIsAboveTarget) return;
+
+      const draggedItemComesAfter = draggedIndex > targetIndex;
+      const draggedItemIsBelowTarget = draggedTop > targetCenter
+
+      if (draggedItemComesAfter && draggedItemIsBelowTarget) return;
+
+      console.log('test')
+      // if (draggedIndex < tar)
+    },
+    
+  })
+
+
+  dragRef(dropRef(ref))
+
+
   return (
-    <Container ref={dragRef} isDraggin={isDraggin}>
+    <Container ref={ref} isDraggin={isDraggin}>
       <header>
         {data.labels.map((label) => (
           <Label key={label} color={label} />
